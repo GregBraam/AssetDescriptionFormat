bd102eacbc8263dfcc47f9c105613227fb31ae1c
GregBraam <52622160+GregBraam@users.noreply.github.com>
2024-11-24 20:01:47 +0000

    Implement Import/Export Helper fields.

--- blenderaddon/adf_export.py
+++ blenderaddon/adf_export.py
@@ -1,14 +1,41 @@
 
+
 import bpy
+from bpy_extras.io_utils import ExportHelper
 from . import adf_utils
 
-class ExportADFOperator(bpy.types.Operator):
+
+class ExportADFOperator(bpy.types.Operator, ExportHelper):
     bl_idname = "export_mesh.adf"
     bl_label = "Export ADF"
+
+    # default extension
+    filename_ext = ".adf"
+
+    filter_glob: bpy.props.StringProperty(
+        default="*.adf",
+        options={"HIDDEN"},
+        maxlen=255
+    ) # type: ignore
+
+    export_materials: bpy.props.BoolProperty(
+        name = "Export Materials",
+        description = "Include materials and textures in export",
+        default=True
+    ) # type: ignore
+
+    export_selection: bpy.props.BoolProperty(
+        name = "Export Selection",
+        description = "Export only selected objects",
+        default=True
+    ) # type: ignore
+
     def execute(self, context):
 
+        # implemented by ExportHelper
+        path = self.filepath
         # Need to add export options
 
-        adf_utils.adf_write("file.adf")
+        adf_utils.adf_write(path)
         return {"FINISHED"}
 
--- blenderaddon/adf_import.py
+++ blenderaddon/adf_import.py
@@ -1,10 +1,18 @@
 
 import bpy
+from bpy_extras.io_utils import ImportHelper
 from . import adf_utils
 
-class ImportADFOperator(bpy.types.Operator):
+class ImportADFOperator(bpy.types.Operator, ImportHelper):
     bl_idname = "import_mesh.adf"
     bl_label = "Import ADF"
+
+    filename_ext = ".adf"
+    filter_glob: bpy.props.StringProperty(
+        default="*.adf",
+        options={"HIDDEN"},
+        maxlen=255
+    ) # type: ignore
 
     def execute(self,context):
         # Import options
