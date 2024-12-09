b1e147ef166f46ff659cf96e8a814cc974778d32
GregBraam <52622160+GregBraam@users.noreply.github.com>
2024-12-09 11:22:43 +0000

    Finish converting material links and nodes to json.

--- blenderaddon/__init__.py
+++ blenderaddon/__init__.py
@@ -2,7 +2,7 @@
 import bpy
 from .adf_export import ExportADFOperator
 from .adf_import import ImportADFOperator
-from .experiment_operators import SerializeNodesOperator
+from .experiment_operators import SerializeMaterialOperator
 
 def add_import_menu(self,context):
     self.layout.operator(ImportADFOperator.bl_idname, text = "ADF (.adf)")
@@ -13,13 +13,12 @@ def add_export_menu(self,context):
 classes = (
     ExportADFOperator,
     ImportADFOperator,
-    SerializeNodesOperator
+    SerializeMaterialOperator,
 )
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-
 
     bpy.types.TOPBAR_MT_file_import.append(add_import_menu)
     bpy.types.TOPBAR_MT_file_export.append(add_export_menu)
--- blenderaddon/experiment_operators.py
+++ blenderaddon/experiment_operators.py
@@ -1,13 +1,19 @@
 import bpy
+import json
 from .adf_write import get_materials
+from . import experiment_utils
 
-class SerializeNodesOperator(bpy.types.Operator):
-    bl_idname = "experiment.serialize_nodes"
-    bl_label = "Serialize Nodes"
+class SerializeMaterialOperator(bpy.types.Operator):
+    bl_idname = "experiment.serialize_material"
+    bl_label = "Serialize Material"
     def execute(self,context):
-        print("Serialize Nodes")
-
         selection = bpy.context.selected_objects
         materials = get_materials(selection)
+
+        serialized_nodes = experiment_utils.serialize_all_material_nodes(materials)
+        serialized_links = experiment_utils.serialize_all_material_links(materials)
+
+        print(f"\nMaterial links:{serialized_links}")
+        print(f"\nMaterial nodes:{serialized_nodes}")
 
         return {"FINISHED"}
