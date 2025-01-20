310b8e7d54275a06b0d6f4867e81e7f366b4e5d5
GregBraam <52622160+GregBraam@users.noreply.github.com>
2025-01-20 17:48:21 +0000

    Collect all chunks, rather than types of chunks at a time.

--- blenderaddon/adf_read.py
+++ blenderaddon/adf_read.py
@@ -2,8 +2,9 @@ import bpy # type: ignore[import-untyped]
 from . import adf_utils
 from .adf_utils import FILE_HEADER_MAGIC
 from .header_data import HeaderData
-from .model_data import ModelData
+from .generic_chunk_data import GenericChunkData
 from .adf_log import log
+import tempfile, os
 
 import string
 
@@ -12,16 +13,35 @@ def adf_read(file_path: str):
         data = file.read()
 
     header_data = __get_header_data(data)
-    model_data = __get_model_data(data)
-
-    textures_data = __get_all_texture_data(data,header_data,model_data)
     
-
-    # get textures and instantiate images in blender
-    # get materials data\
-    # instantiate material, instantiate nodes and then connect them with links
-
+    #Collect all chunks
+    all_chunks = __get_all_data_chunks_from_data(data,header_data)
+    
+    #Organize chunks, then instantaite them.
     return
+
+def __get_all_data_chunks_from_data(data: bytes,header: HeaderData) -> list[GenericChunkData]:
+    chunks_collection: list[GenericChunkData] = []
+
+    # number of chunks is models (always 1) + textures (variable) + mat links(1) + mat nodes(1) + tex names (1)
+    total_chunks_count = header.textures + 4
+
+    offset = 17
+    for i in range(0,total_chunks_count):
+        chunk = __get_chunk_from_data(data,offset)
+        chunks_collection.append(chunk)
+        offset = offset + len(chunk.chunk_data) + 9
+
+    return chunks_collection
+
+def __get_chunk_from_data(data: bytes, offset: int) -> GenericChunkData:
+    c_length = int.from_bytes(data[offset:offset+4],byteorder="little")
+    c_id = int.from_bytes(data[offset+4:offset+8],byteorder="little")
+    c_type = data[offset+8]
+    c_data = data[offset+8:offset+8+c_length]
+
+    chunk = GenericChunkData(c_length,c_id,c_type,c_data)
+    return chunk
 
 def __get_header_data(data: bytes) -> HeaderData:
     """Gets the header chunk data from the bytes of an ADF file."""
@@ -33,63 +53,17 @@ def __get_header_data(data: bytes) -> HeaderData:
 
     return HeaderData(magic,version,models,textures,materials)
 
-def __get_model_data(data: bytes) -> ModelData:
-    """Get all model data from the bytes of an ADF file."""
-    chunk_length = int.from_bytes(data[17:21],byteorder="little")
-    chunk_identifier = int.from_bytes(data[21:25],byteorder="little")
-    chunk_type = data[25]
-    chunk_data = data[25:25+chunk_length]
-
-    return ModelData(chunk_length,chunk_identifier,chunk_type,chunk_data)
-
-def __get_all_texture_data(data: bytes,header_data: HeaderData,model_data: ModelData) -> list[bytes]:
-    """Get all textures from an adf file"""
-
-    log(message=f"Getting: {header_data.textures} textures")
-    
-    texture_data_collection: list[bytes] = []
-
-    texture_offset = model_data.chunk_length + 26
-    for i in range(0,header_data.textures):
-        tex_data = __get_texture_data(data,texture_offset)
-        texture_data_collection.append(tex_data)
-        texture_offset = texture_offset + len(tex_data)
-        # add to offset to get to next file
-
-    return texture_data_collection
-
-def __get_texture_data(data: bytes, offset: int) -> bytes:
-    """Get a single texture from adf file"""
-    chunk_length = int.from_bytes(data[offset:offset+4],byteorder="little")
-
-    log(message="Getting tex data!")
-
-    chunk_identifier = int.from_bytes(data[offset+4:offset+8],byteorder="little")
-    chunk_type = data[offset+8]
-    chunk_data = data[offset+8:offset+8+chunk_length]
-    
-    return chunk_data
-
 def __instantiate_images(texture_data_collection: list[bytes]):
     for tex in texture_data_collection:
         print(tex)
 
 def __instantiate_image(texture_data: bytes):
-    return
+    with tempfile.NamedTemporaryFile(suffix=".file", delete=False) as temp_file:
+        temp_file_path = temp_file.name
 
-def __get_all_material_data():
-    """Get all materials"""
-    # Will need an offset for this
-    return
+    with open(temp_file_path,"wb") as f:
+        f.write(texture_data)
 
-def __get_material_data():
-    """Get a single material"""
-    return
+    bpy.ops.image.open(filepath=temp_file_path)
 
-def __get_nodes_data():
-    """Get the node chunk data."""
-    return
-
-def __get_links_data():
-    """Get the links chunk data."""
     return
--- blenderaddon/generic_chunk_data.py
+++ blenderaddon/generic_chunk_data.py
@@ -1,7 +1,8 @@
 from typing import NamedTuple
 
-class ModelData(NamedTuple):
+class GenericChunkData(NamedTuple):
     chunk_length: int
     chunk_identifier: int
     chunk_type: int
     chunk_data: bytes
+    
